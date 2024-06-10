f53d48079d03c56a2a384ff75bc337d4a6f377e7
yamada-ai <hatsudaaa@gmail.com>
2024-06-10 13:56:44 +0900

    一旦諦めた．TODO : js auto

---
+++ src/main/java/com/example/controller/EmployeeAPIController.java
@@ -0,0 +1,30 @@
+package com.example.controller;
+
+import com.example.domain.Employee;
+import com.example.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/employee/api")
+public class EmployeeAPIController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @GetMapping("/search")
+    public List<String> searchAPI(@RequestParam String query){
+        System.out.println(query);
+        List<Employee> employeeList = employeeService.searchByNameLike(query);
+        return employeeList.stream()
+                .map(Employee::getName)
+                .collect(Collectors.toList());
+    }
+}
---
+++ src/main/resources/static/js/auto.js
@@ -0,0 +1,24 @@
+$(function () {
+  //   $("#autocomplete").on("keyup", function () {
+  //     $.ajax({
+  //       url: "http://localhost:8080/employee/api/search", // REST APIのエンドポイント
+  //       type: "GET",
+  //       dataType: "json",
+  //       data: {
+  //         query: $("#autocomplete").val(),
+  //       },
+  //       async: true,
+  //     })
+  //       .done(function (data) {
+  //         // コンソールに取得データを表示
+  //         console.log(data);
+  //         console.dir(JSON.stringify(data));
+  //       })
+  //       .fail(function (XMLHttpRequest, textStatus, errorThrown) {
+  //         alert("エラーが発生しました！");
+  //         console.log("XMLHttpRequest : " + XMLHttpRequest.status);
+  //         console.log("textStatus     : " + textStatus);
+  //         console.log("errorThrown    : " + errorThrown.message);
+  //       });
+  //   });
+});
